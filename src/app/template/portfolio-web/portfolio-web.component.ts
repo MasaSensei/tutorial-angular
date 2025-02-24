28183796c232a2a1276cdaf526707362d72995a1
Masayoshi <masasensei26@mail.com>
2025-02-24 16:46:01 +0700

    Section 6: Membuat Kata Sambutan Component

--- src/app/template/portfolio-web/portfolio-web.component.ts
+++ src/app/template/portfolio-web/portfolio-web.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '@components/portofolio-web/header/header.component';
 import { BannerComponent } from '@components/portofolio-web/banner/banner.component';
 import { HeaderMenuComponent } from '@components/portofolio-web/header-menu/header-menu.component';
+import { AboutUsComponent } from '@components/portofolio-web/about-us/about-us.component';
 
 @Component({
   selector: 'app-portfolio-web',
   standalone: true,
-  imports: [HeaderComponent, BannerComponent, HeaderMenuComponent],
+  imports: [
+    HeaderComponent,
+    BannerComponent,
+    HeaderMenuComponent,
+    AboutUsComponent,
+  ],
   templateUrl: './portfolio-web.component.html',
   styleUrl: './portfolio-web.component.css',
 })
